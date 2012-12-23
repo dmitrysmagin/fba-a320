cda8e4b5325074e33c037dda59fcab10cddc341d
Dmitry Smagin <exmortis@yandex.ru>
2012-12-23 18:04:12 +0400

    Remove hack that forced all cps1 games to have sound in 11kHz mono: reimport ym2151.c, burn_sound_c.cpp, burn_ym2151.cpp and msm6295.h from mainline FBA 0.2.96.72; also fix burn/capcom/ps_m.cpp. As a result, samplerate for cps1 games can be set up to 44100 with no crash. It seems as well that non-mmx mixing code in burn_ym2151.cpp was broken all the time (wrongly applied YM2151Volume caused an overclip), hackfix it.

--- burn/burn_ym2151.cpp
+++ burn/burn_ym2151.cpp
@@ -78,24 +78,26 @@ static void YM2151RenderResample(short* pSoundBuf, int nSegmentLength)
 
 static void YM2151RenderNormal(short* pSoundBuf, int nSegmentLength)
 {
+	nBurnPosition += nSegmentLength;
+
 	pYM2151Buffer[0] = pBuffer;
 	pYM2151Buffer[1] = pBuffer + nSegmentLength;
 
 	YM2151UpdateOne(0, pYM2151Buffer, nSegmentLength);
-	{
-		for (int n = nSegmentLength; n > 0; n--) {
-			*pSoundBuf++ = *pYM2151Buffer[0]++;
-			*pSoundBuf++ = *pYM2151Buffer[1]++;
+
+#ifndef OOPSWARE_FIX
+	if (bBurnUseMMX) {
+		BurnSoundCopy_FM_A(pYM2151Buffer[0], pYM2151Buffer[1], pSoundBuf, nSegmentLength, nYM2151Volume, nYM2151Volume);
+	} else {
+#endif
+		for (int n = 0; n < nSegmentLength; n++) {
+			pSoundBuf[(n << 1) + 0] = (pYM2151Buffer[0][n]  * nYM2151Volume) >> 12; // This code is broken in original FBA SDL
+			pSoundBuf[(n << 1) + 1] = (pYM2151Buffer[1][n]  * nYM2151Volume) >> 12; // that's why nYM2151Volume is altered to hackfix it
 		}
+#ifndef OOPSWARE_FIX
 	}
-
+#endif
 }
-
-static void YM2151RenderMono(short* pSoundBuf, int nSegmentLength)
-{
-	YM2151UpdateOne(0, &pSoundBuf, nSegmentLength);
-}
-
 
 void BurnYM2151Reset()
 {
@@ -109,36 +111,34 @@ void BurnYM2151Exit()
 	free(pBuffer);
 }
 
-int BurnYM2151Init(int nClockFrequency, float nVolume, bool stereo)
+int BurnYM2151Init(int nClockFrequency, float nVolume)
 {
 	if (nBurnSoundRate <= 0) {
-		YM2151Init(1, nClockFrequency, 11025,stereo?1:0);
+		YM2151Init(1, nClockFrequency, 11025);
 		return 0;
 	}
 
-	
-	if (nFMInterpolation == 3 || nBurnSoundRate > 11025) {
-		if (nBurnSoundRate > 11025)
-			nBurnYM2151SoundRate = 11025;
-		else
-			nBurnYM2151SoundRate = nBurnSoundRate;
-		if (nFMInterpolation == 3)
-			nBurnYM2151SoundRate = nBurnYM2151SoundRate >> 1;
+	if (nFMInterpolation == 3) {
+		// Set YM2151 core samplerate to match the hardware
+		nBurnYM2151SoundRate = nClockFrequency >> 6;
+		// Bring YM2151 core samplerate within usable range
+		while (nBurnYM2151SoundRate > nBurnSoundRate * 3) {
+			nBurnYM2151SoundRate >>= 1;
+		}
 
 		BurnYM2151Render = YM2151RenderResample;
 
 		nYM2151Volume = (int)((double)16384.0 * 100.0 / nVolume);
 	} else {
 		nBurnYM2151SoundRate = nBurnSoundRate;
-		if (stereo)
-			BurnYM2151Render = YM2151RenderNormal;
-		else
-			BurnYM2151Render = YM2151RenderMono;
-		
-		nYM2151Volume = (int)((double)65536.0 * 100.0 / nVolume);
-	}
+		BurnYM2151Render = YM2151RenderNormal;
 
-	YM2151Init(1, nClockFrequency, nBurnYM2151SoundRate,stereo?1:0);
+		nYM2151Volume = (int)((double)65536.0 * 100.0 / nVolume);
+	} 
+
+	nYM2151Volume = (int)((float)(60.0 * nVolume)); // hackfix overclip, non-mmx mixing code is broken in original FBASDL
+
+	YM2151Init(1, nClockFrequency, nBurnYM2151SoundRate);
 
 	pBuffer = (short*)malloc(65536 * 2 * sizeof(short));
 	memset(pBuffer, 0, 65536 * 2 * sizeof(short));
